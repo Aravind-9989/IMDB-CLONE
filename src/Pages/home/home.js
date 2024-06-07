7928bd8d43ab1ff87d7d7c3fbca0246d746aeae6
Aravind-9989 <shetty2160@gmail.com>
2024-06-07 15:10:20 +0530

    updated changes @

--- src/Pages/home/home.js
+++ src/Pages/home/home.js
@@ -1,3 +1,182 @@
+// import React, { useEffect, useState } from "react";
+// import "react-responsive-carousel/lib/styles/carousel.min.css";
+// import { Carousel } from "react-responsive-carousel";
+// import { Link, useNavigate } from "react-router-dom";
+// import MovieList from "../../components/movieList/movieList";
+// import { getAuth, onAuthStateChanged, signOut } from "firebase/auth";
+// import { app } from "../../firebase/firebase";
+
+// function Home() {
+//   const [popularMovies, setPopularMovies] = useState([]);
+//   const nav = useNavigate();
+//   const [user, setUser] = useState("");
+//   const auth = getAuth(app);
+
+//   useEffect(() => {
+//     fetch(
+//       "https://api.themoviedb.org/3/movie/popular?api_key=4e44d9029b1270a757cddc766a1bcb63&language=en-US"
+//     )
+//       .then((res) => res.json())
+//       .then((data) => {
+//         console.log(data.results);
+//         setPopularMovies(data.results);
+//       })
+//       .catch((error) => {
+//         console.error("Error fetching popular movies:", error);
+//       });
+//   }, []);
+
+//   useEffect(() => {
+//     onAuthStateChanged(auth, (u) => {
+//       if (u) {
+//         setUser(u);
+//       }
+//     });
+//   }, [auth]);
+
+//   const log = () => {
+//     nav("/login");
+//   };
+
+//   const sig = () => {
+//     nav("/signup");
+//   };
+
+//   const logo = () => {
+//     signOut(auth).then(() => {
+//       console.log("Successfully signed out");
+//       nav("/login");
+//     });
+//   };
+
+//   return (
+//     <div className="poster">
+//       {user ? (
+//         <button className="Btn" onClick={logo}>
+//           Logout
+//         </button>
+//       ) : (
+//         <>
+//           <div
+//             style={{
+//               position: "absolute",
+//               top: "10px",
+//               right: "60px",
+//               display: "flex",
+//               alignItems: "center",
+//               textAlign: "center",
+//             }}
+//           >
+//             {/* <button
+//               className="Btn"
+//               onClick={log}
+//               style={{
+//                 height: "40px",
+//                 marginRight: "10px",
+//                 width: "45px",
+//                 borderRadius: "15px",
+//               }}
+//             >
+//               Login
+//             </button>
+//             <button
+//               className="Btnn"
+//               onClick={sig}
+//               style={{
+//                 height: "40px",
+//                 marginRight: "10px",
+//                 borderRadius: "15px",
+//                 width: "50px",
+//               }}
+//             >
+//               SignUp
+//             </button> */}
+//           </div>
+//         </>
+//       )}
+
+//       <Carousel
+//         showThumbs={false}
+//         autoPlay={true}
+//         transitionTime={3}
+//         infiniteLoop={true}
+//         showStatus={false}
+//       >
+//         {popularMovies.map((movie) => (
+//           <Link
+//             style={{ textDecoration: "none", color: "white" }}
+//             to={`/movie/${movie.id}`}
+//             key={movie.id}
+//           >
+//             <div className="posterImage">
+//               <img
+//                 src={`https://image.tmdb.org/t/p/original${
+//                   movie && movie.backdrop_path
+//                 }`}
+//                 alt={movie.title}
+//               />
+//             </div>
+//             <div style={{ marginTop: "-520px", position: "absolute" }}>
+//               <div
+//                 className="posterImage__overlay"
+//                 style={{ padding: "20px", backgroundColor: "transparent" }}
+//               >
+//                 <div style={{ marginBottom: "10px" }}>
+//                   <div
+//                     className="posterImage__title"
+//                     style={{
+//                       fontSize: "45px",
+//                       color: "white",
+//                       textAlign: "left",
+//                     }}
+//                   >
+//                     {movie ? movie.original_title : ""}
+//                   </div>
+//                   <div
+//                     className="posterImage__runtime"
+//                     style={{
+//                       fontSize: "30px",
+//                       color: "white",
+//                       textAlign: "left",
+//                     }}
+//                   >
+//                     {movie ? movie.release_date : ""}
+//                     <span
+//                       className="posterImage__rating"
+//                       style={{ marginLeft: "20px", fontSize: "30px" }}
+//                     >
+//                       {movie ? movie.vote_average : ""}
+//                       <i
+//                         className="fas fa-star"
+//                         style={{ color: "#e6b706", marginLeft: "10px" }}
+//                       />
+//                     </span>
+//                   </div>
+//                 </div>
+//                 <div
+//                   className="posterImage__description"
+//                   style={{
+//                     fontSize: "25px",
+//                     color: "white",
+//                     textAlign: "left",
+//                   }}
+//                 >
+//                   {movie ? movie.overview : ""}
+//                 </div>
+//               </div>
+//             </div>
+//           </Link>
+//         ))}
+//       </Carousel>
+//       <MovieList />
+//     </div>
+//   );
+// }
+
+// export default Home;
+
+// hi this is updated page
+
 import React, { useEffect, useState } from "react";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from "react-responsive-carousel";
@@ -5,6 +184,7 @@ import { Link, useNavigate } from "react-router-dom";
 import MovieList from "../../components/movieList/movieList";
 import { getAuth, onAuthStateChanged, signOut } from "firebase/auth";
 import { app } from "../../firebase/firebase";
+import "./home.css"; 
 
 function Home() {
   const [popularMovies, setPopularMovies] = useState([]);
@@ -56,43 +236,18 @@ function Home() {
           Logout
         </button>
       ) : (
-        <>
-          <div
-            style={{
-              position: "absolute",
-              top: "10px",
-              right: "60px",
-              display: "flex",
-              alignItems: "center",
-              textAlign: "center",
-            }}
-          >
-            {/* <button
-              className="Btn"
-              onClick={log}
-              style={{
-                height: "40px",
-                marginRight: "10px",
-                width: "45px",
-                borderRadius: "15px",
-              }}
-            >
-              Login
-            </button>
-            <button
-              className="Btnn"
-              onClick={sig}
-              style={{
-                height: "40px",
-                marginRight: "10px",
-                borderRadius: "15px",
-                width: "50px",
-              }}
-            >
-              SignUp
-            </button> */}
-          </div>
-        </>
+        <div
+          style={{
+            position: "absolute",
+            top: "10px",
+            right: "60px",
+            display: "flex",
+            alignItems: "center",
+            textAlign: "center",
+          }}
+        >
+          {/* Login and Signup buttons */}
+        </div>
       )}
 
       <Carousel
@@ -110,59 +265,23 @@ function Home() {
           >
             <div className="posterImage">
               <img
-                src={`https://image.tmdb.org/t/p/original${
-                  movie && movie.backdrop_path
-                }`}
+                src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
                 alt={movie.title}
               />
             </div>
-            <div style={{ marginTop: "-520px", position: "absolute" }}>
-              <div
-                className="posterImage__overlay"
-                style={{ padding: "20px", backgroundColor: "transparent" }}
-              >
-                <div style={{ marginBottom: "10px" }}>
-                  <div
-                    className="posterImage__title"
-                    style={{
-                      fontSize: "45px",
-                      color: "white",
-                      textAlign: "left",
-                    }}
-                  >
-                    {movie ? movie.original_title : ""}
-                  </div>
-                  <div
-                    className="posterImage__runtime"
-                    style={{
-                      fontSize: "30px",
-                      color: "white",
-                      textAlign: "left",
-                    }}
-                  >
-                    {movie ? movie.release_date : ""}
-                    <span
-                      className="posterImage__rating"
-                      style={{ marginLeft: "20px", fontSize: "30px" }}
-                    >
-                      {movie ? movie.vote_average : ""}
-                      <i
-                        className="fas fa-star"
-                        style={{ color: "#e6b706", marginLeft: "10px" }}
-                      />
-                    </span>
-                  </div>
-                </div>
-                <div
-                  className="posterImage__description"
-                  style={{
-                    fontSize: "25px",
-                    color: "white",
-                    textAlign: "left",
-                  }}
-                >
-                  {movie ? movie.overview : ""}
-                </div>
+            <div className="posterImage__overlay">
+              <div className="posterImage__title">
+                {movie.original_title}
+              </div>
+              <div className="posterImage__runtime">
+                {movie.release_date}
+                <span className="posterImage__rating">
+                  {movie.vote_average}
+                  <i className="fas fa-star" />
+                </span>
+              </div>
+              <div className="posterImage__description">
+                {movie.overview}
               </div>
             </div>
           </Link>
--- src/components/card/card.js
+++ src/components/card/card.js
@@ -41,3 +41,10 @@ console.log(isLoading)
 }
 
 export default Cards
+
+
+
+
+
+
+
